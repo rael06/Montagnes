58407c9e4d6a0642e8a840168053e0b6747150e5
rael06 <47027233+rael06@users.noreply.github.com>
2019-02-22 11:26:09 +0100

    Add files via upload

---
+++ On-va-deplacer-des-montagnes-master/assets/js/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+(function() {
+    $(document).ready(function() {
+        $('#dismiss, .overlay').on('click', function() {
+            // cacher le menu (sidebar)
+            $('#sidebar').removeClass('active');
+            // cacher l'ombre (utilisation special overlay)
+            $('.overlay').removeClass('active');
+        });
+
+        $('#sidebarCollapse').on('click', function() {
+            // ouvrir
+            $('#sidebar').addClass('active');
+            $('.overlay').addClass('active');
+        });
+    });
+
+    jQuery(function($){
+        var pictures = $('.pictures');
+        pictures.masonry({
+            itemSelector: '.pic',
+            columnWidth: 1,
+        });
+    });
+})()
