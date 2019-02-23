535b4c874add35ab072254dc916417a8f504cd63
rael06 <47027233+rael06@users.noreply.github.com>
2019-02-23 11:37:09 +0100

    Add files via upload

--- On-va-deplacer-des-montagnes-master/assets/js/main.js
+++ On-va-deplacer-des-montagnes-master/assets/js/main.js
@@ -3,24 +3,40 @@
 (function() {
     $(document).ready(function() {
         $('#dismiss, .overlay').on('click', function() {
-            // cacher le menu (sidebar)
+            // hide menu (sidebar)
             $('#sidebar').removeClass('active');
-            // cacher l'ombre (utilisation special overlay)
+            // hide shadow (use special overlay)
             $('.overlay').removeClass('active');
         });
 
         $('#sidebarCollapse').on('click', function() {
-            // ouvrir
+            // open
             $('#sidebar').addClass('active');
             $('.overlay').addClass('active');
         });
     });
 
-    jQuery(function($){
+    function photos_orderer() {
         var pictures = $('.pictures');
         pictures.masonry({
             itemSelector: '.pic',
-            columnWidth: 1
+            columnWidth: 1,
         });
+    };
+    photos_orderer();
+
+    // Scroll to Top
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200); // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200); // Else fade out the arrow
+        }
+    });
+
+    $('#return-to-top').on("click", function() { // When arrow is clicked
+        $('body,html').animate({
+            scrollTop: 0 // Scroll to top of body
+        }, 5000);
     });
 })()
